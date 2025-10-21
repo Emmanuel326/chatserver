aca73ea71fca7e869f78d789462e471d2dc670a7
Robert <titus.robert@outlook.com>
2025-10-21 18:38:59 +0800

    fix(db): switched to mordernc.org/sqlite driver and changed driver name

--- internal/ports/sqlite/db.go
+++ internal/ports/sqlite/db.go
@@ -5,20 +5,18 @@ import (
 
 	"github.com/Emmanuel326/chatserver/internal/config"
 	"github.com/jmoiron/sqlx"
-	_ "github.com/mattn/go-sqlite3" // SQLite driver
+	_ "modernc.org/sqlite" // SQLite driver
 )
 
 // InitDB initializes the database connection using the provided configuration.
 func InitDB(cfg *config.Config) *sqlx.DB {
-	// sqlx.Connect is a helper that calls sql.Open() and then db.Ping()
-	// The driver is "sqlite3" and the DSN is the file path.
-	db, err := sqlx.Connect("sqlite3", cfg.DB_FILE)
+	// Connect using the modernc.org/sqlite driver
+	db, err := sqlx.Connect("sqlite", cfg.DB_FILE) // <- driver name must be "sqlite"
 	if err != nil {
 		log.Fatalf("FATAL: Could not connect to the SQLite database: %v", err)
 	}
 
 	// SQLite-specific: enable foreign key constraints for safety
-	// Must be run every time a connection is opened.
 	if _, err := db.Exec("PRAGMA foreign_keys = ON;"); err != nil {
 		log.Fatalf("FATAL: Could not enable foreign keys for SQLite: %v", err)
 	}
